640a9aea3dd405c81c7af8ccc01599621635e467
mvskakashi <marco.simone@atlantica.it>
2022-03-30 15:01:34 +0200

    atomic function for ctf selenium

--- scripts/bootctf.js
+++ scripts/bootctf.js
@@ -3,6 +3,7 @@ const firefox = require('selenium-webdriver/firefox');
 class BootCtf {
   
   static async create() {
+    //npm run test --dossier=867673 --username=20403680 --password=123457
     let options = new firefox.Options().setPreference('security.tls.version.min', 1);
     let driver = await new Builder().forBrowser('firefox')
       .setFirefoxOptions(options)
@@ -21,6 +22,11 @@ class BootCtf {
     throw new Error("New window did not appear before timeout")
   }
 
+  static async debugWin(driver, txt, second) {
+    driver.executeScript("window.open('', '" + txt + "', 'width=200,height=100')")
+    await driver.sleep(second * 1000)
+  }
+
   static async logElements(elem, logTrace, second) {
     for (let i = 0; i < elem.length; i++) {
       el = elem[i]
@@ -35,22 +41,44 @@ class BootCtf {
     await wait(second * 1000)
   }
 
-  static async login(driver, vars, username, password) {
+  static async focusOnWindows(win, vars) {
+    await driver.switchTo().window(vars[win])
+  }
 
+  static async login(driver, vars, username, password) {
+      console.log("start login")
       await driver.get("https://172.21.160.241/eflow/LoginServlet")
       await driver.sleep(3000)
       await driver.findElement(By.id("txtUID")).sendKeys(username)
       await driver.findElement(By.id("txtPsw")).sendKeys(password)
       vars["windowHandles"] = await driver.getAllWindowHandles()
       await driver.findElement(By.id("cmdConferma")).click()
-      vars["win9803"] = await BootCtf.waitForWindow(driver, vars, 5000)
-      vars["root"] = await driver.getWindowHandle()
-      await driver.switchTo().window(vars["win9803"])
-      await driver.sleep(5000)
+      vars["W_ROOT"] = await BootCtf.waitForWindow(driver, vars, 10000)
+      vars["W_LOGIN"] = await driver.getWindowHandle()
+      await driver.switchTo().window(vars["W_ROOT"])
+      console.log("end login")
       
   }
 
-  static async openDossier(driver, vars, dossier) {
+  static async openDetailsDossier(driver, vars, dossier) {
+    console.log("start openDetailsDossier")
+    await BootCtf.openWorkingTasks(driver, vars, dossier)
+    await BootCtf.searchDossier(driver, vars, dossier)
+    await BootCtf.gotoCtfDom(driver, vars)
+
+    driver.switchTo().frame(0) // frame id = Lista
+    let elemchild = await driver.findElement(By.css("td:nth-child(2)"))//.click()
+    elemchild.click()
+    await driver.sleep(8000)
+    driver.switchTo().parentFrame()
+    driver.switchTo().frame(1); // frame name = BODY
+    console.log("end openDetailsDossier")
+  }
+
+  static async openWorkingTasks(driver, vars, dossier) {
+    console.log("start openWorkingTasks")
+    await driver.switchTo().window(vars["W_ROOT"])
+    driver.switchTo().defaultContent()
     await driver.findElement(By.id("header")).click();
     // 18 | sendKeys | id=menuSearch | ${KEY_DOWN}
     await driver.findElement(By.id("menuSearch")).click();
@@ -60,20 +88,20 @@ class BootCtf {
     await driver.findElement(By.id("menuSearch")).sendKeys(Key.DOWN);
     await driver.findElement(By.id("menuSearch")).sendKeys(Key.ENTER);
     await driver.sleep(8000);
+    console.log("end openWorkingTasks")
+  }
 
+  static async searchDossier(driver, vars, dossier) {
+    console.log("start searchDossier")
+    await driver.switchTo().window(vars["W_ROOT"])
+    driver.switchTo().defaultContent()
     await driver.switchTo().frame(1)
-
     await driver.switchTo().frame(1)
-
     await driver.switchTo().frame(1)
-
     await driver.findElement(By.xpath("//img[@title=\'Search Dossier\']")).click()
     await driver.sleep(5000)
-
     await driver.switchTo().parentFrame()
-    
     await driver.switchTo().frame(2)
-
     await driver.findElement(By.id("txtPratNumEsatto")).click()
     await driver.findElement(By.id("txtPratNumEsatto")).sendKeys(dossier) //867673
     //	await driver.findElement(By.id("txtPratNumEsatto")).sendKeys(dossierNumber)
@@ -85,7 +113,23 @@ class BootCtf {
       await driver.actions({ bridge: true }).doubleClick(element).perform()
     }
     await driver.sleep(3000)
-    
+    console.log("end searchDossier")
+  }
+
+  static async gotoCtfDom(driver, vars) {
+    console.log("start gotoCtfDom")
+    await driver.switchTo().window(vars["W_ROOT"])
+    await driver.switchTo().defaultContent()
+    await driver.sleep(2000)
+    let frBody = await driver.findElement(By.id("frBody"));
+    driver.switchTo().frame(frBody);
+    let elem = await driver.findElements(By.tagName("iframe"))
+    let elem2 = await driver.findElements(By.tagName("frame"))
+    driver.switchTo().frame(1); // frame id = frMain
+    driver.switchTo().frame(2) // frame name = MainForm (ctf frame)
+    console.log("inner ctf dom")
+    await driver.sleep(2000)
+    console.log("end gotoCtfDom")
   }
 }
 
